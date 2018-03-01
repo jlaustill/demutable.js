ae269b2dc99fdf6f1210227b7eaa1151db1bcdd6
Joshua Austill <jaustill@sofi.org>
2018-02-28 18:46:10 -0700

    Add more tests and array methods

--- src/demute-array.js
+++ src/demute-array.js
@@ -1,13 +1,33 @@
+import demute from '../index';
 const _ = require('lodash');
 
 const demuteArray = function (obj) {
     if (_.isArray(obj)) {
-        obj.push = function(value) {
-            return [...this, value];
+        obj.push = function(...values) {
+            return demute([...this, ...values]);
         };
 
         obj.pop = function() {
-            return this[this.length - 1];
+            return demute(this[this.length - 1]);
+        };
+
+        obj.shift = function() {
+            return this.slice(0, 1)[0];
+        };
+
+        // obj.sort
+
+        obj.reverse = function() {
+            return demute(this.map((_, i, a) => a[a.length - i - 1]));
+        };
+
+        // obj.splice
+        obj.splice = function(start, end, ...adds) {
+            return demute([...this.slice(0, start), ...adds, ...this.slice(start + end)]);
+        };
+
+        obj.unshift = function(...values) {
+            return demute([...values, ...this]);
         }
     }
 
--- test/array.test.js
+++ test/array.test.js
@@ -1,13 +1,50 @@
 import demute, { isDemuted } from '../index';
+const a = demute([1,3,2]);
 
 describe('array', () => {
-  it('should pop without mutating', () => {
-    const a = demute([1,2,3]);
-    const b = a.pop();
-
-    expect(a.length).toBe(3);
-    expect(b).toBe(3);
-    expect(isDemuted(a)).toBe(true);
+  it('should splice without mutating', () => {
+    const b = a.splice(1,1,4,5);
+    expect(b.length).toBe(4);
     expect(isDemuted(b)).toBe(true);
+  });
+
+  it('should reverse by returning, not mutating', () => {
+    const b = a.reverse();
+    expect(b.pop()).toBe(1);
+    expect(b.shift()).toBe(2);
+    expect(isDemuted(b)).toBe(true);
+  });
+
+  it('should pop without mutating', () => {
+    const b = a.pop();
+    expect(b).toBe(2);
+    expect(isDemuted(b)).toBe(true);
+  });
+
+  it('should push without mutating', () => {
+    const b = a.push(4);
+    expect(b.length).toBe(4);
+    expect(isDemuted(b)).toBe(true);
+  });
+
+  it('should shift without mutating', () => {
+    const b = a.shift();
+    expect(b).toBe(1);
+    expect(isDemuted(b)).toBe(true);
+  });
+
+  it('should unshift without mutating', () => {
+    const b = a.unshift(4, 5);
+    expect(b.length).toBe(5);
+    expect(b).toContain(5);
+    expect(isDemuted(b)).toBe(true);
+  });
+
+  it('should not mutate under any circumstances', () => {
+    expect(a.length).toBe(3);
+  });
+
+  it('should remain demuted', () => {
+    expect(isDemuted(a)).toBe(true);
   });
 });
