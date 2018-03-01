3a4bf6fe3f554967b61624a51fe11d1bb8c8e5d6
Joshua Austill <jaustill@sofi.org>
2018-02-28 23:00:45 -0700

    small changes to improve use with functions

--- src/demute.js
+++ src/demute.js
@@ -2,17 +2,20 @@ import demuteArray from './demute-array';
 
 const demute = function (_obj) {
   const obj = demuteArray(_obj);
+  const objectIsFunction = typeof obj === 'function';
 
   // Retrieve the property names defined on obj
   const propNames = Object.getOwnPropertyNames(obj);
 
   // Freeze properties before freezing self
   propNames.forEach(function(name) {
-    let prop = obj[name];
-
-    // Freeze prop if it is an object
-    if (typeof prop === 'object' && prop !== null)
+    const prop = obj[name];
+    if ((objectIsFunction ? name !== 'caller' && name !== 'callee' && name !== 'arguments' : true)
+    && prop !== null
+    && (typeof prop === 'object' || prop === 'function')
+      && !Object.isFrozen(prop)) {
       demute(prop);
+    }
   });
 
   // Freeze self (no-op if already frozen)
